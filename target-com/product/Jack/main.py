ebcfa99e10f37f6dd8e17eeaac67ec1d072df412
Arif Fadhillah <31968096+ariffadhillah@users.noreply.github.com>
2026-04-24 19:39:58 +0700

    clean: remove sensitive files from history

--- target-com/product/Jack/main.py
+++ target-com/product/Jack/main.py
@@ -22,37 +22,7 @@ ERROR_FILE = f"error{category}-{category_cub}.csv"
 # ============================================================
 
 PROXIES_LIST = [
-    # "166.88.169.235:6842:arssrhsq:x1vpi09f4v1g",
-    # "154.6.129.57:5527:arssrhsq:x1vpi09f4v1g",
-    # "23.236.196.126:6216:arssrhsq:x1vpi09f4v1g",
-    # "50.114.93.3:5987:arssrhsq:x1vpi09f4v1g",
-    # "198.37.121.19:6439:arssrhsq:x1vpi09f4v1g",
-    # "216.173.76.1:6628:arssrhsq:x1vpi09f4v1g",
-    # "173.211.68.189:6471:arssrhsq:x1vpi09f4v1g",
-    # "191.101.181.87:6840:arssrhsq:x1vpi09f4v1g",
-    # "206.206.119.148:6059:arssrhsq:x1vpi09f4v1g",
-    # "206.232.103.193:6350:arssrhsq:x1vpi09f4v1g",
-    # "45.39.4.47:5472:arssrhsq:x1vpi09f4v1g",
-    "23.236.182.223:5999:arssrhsq:x1vpi09f4v1g",
-    "23.27.210.194:6564:arssrhsq:x1vpi09f4v1g",
-    "82.26.238.173:6480:arssrhsq:x1vpi09f4v1g",
-    "104.245.244.64:6504:arssrhsq:x1vpi09f4v1g",
-    "192.3.48.45:6038:arssrhsq:x1vpi09f4v1g",
-    "185.216.105.98:6675:arssrhsq:x1vpi09f4v1g",
-    "45.59.161.140:5932:arssrhsq:x1vpi09f4v1g",
-    "148.135.151.115:8366:arssrhsq:x1vpi09f4v1g",
-    "23.229.125.93:5362:arssrhsq:x1vpi09f4v1g",
-    "104.239.78.204:6149:arssrhsq:x1vpi09f4v1g",
-    "192.3.48.38:6031:arssrhsq:x1vpi09f4v1g",
-    "64.64.118.136:6719:arssrhsq:x1vpi09f4v1g",
-    "23.236.255.5:6781:arssrhsq:x1vpi09f4v1g",
-    "107.172.116.178:5634:arssrhsq:x1vpi09f4v1g",
-    "179.61.245.31:6810:arssrhsq:x1vpi09f4v1g",
-    "23.94.138.138:6412:arssrhsq:x1vpi09f4v1g",
-    "216.173.76.95:6722:arssrhsq:x1vpi09f4v1g",
-    "192.186.151.66:8567:arssrhsq:x1vpi09f4v1g",
-    "45.41.169.251:6912:arssrhsq:x1vpi09f4v1g",
-    "31.58.26.18:6601:arssrhsq:x1vpi09f4v1g"
+   ""
 ]
 
 API_URL = "https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1"
